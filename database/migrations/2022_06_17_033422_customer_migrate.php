8ff841233fecdc9b680429aa8bea9d05916a0308
TNguyenk2 <nguyen.nguyen23@student.passerellesnumeriques.org>
2022-06-21 08:04:13 +0700

    update migration

--- database/migrations/2022_06_17_033422_customer_migrate.php
+++ database/migrations/2022_06_17_033422_customer_migrate.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CustomerMigrate extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,18 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('gender');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phonenumber');
+            $table->string('note');
             $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema:: dropIfExists('customers');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SumController;
-use App\Http\Controllers\SignupController;
-use App\Http\Controllers\RoomController;
-use App\Http\Controllers\PageController;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\SumController;
+// use App\Http\Controllers\SignupController;
+// use App\Http\Controllers\RoomController;
+// use App\Http\Controllers\PageController;
+use App\Http\Controllers\createTable;
+
 
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
@@ -14,12 +16,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Thuc hien duong dan de chay controller
-Route::get('test', [UserController::class , 'xinchao']);
-Route::get('sum', [SumController::class , 'GetValue']);
-Route::post('sum', [SumController::class , 'Result']);
-Route::get('Signup', [SignupController::class , 'index']);
-Route::post('Signup', [SignupController::class , 'displayInfor']);
-Route::get('addRoom', [RoomController::class , 'index']);
-Route::post('addRoom', [RoomController::class , 'addRoom']);
-Route::get('home', [PageController::class , 'getIndex']);
-Route::get('home', [PageController::class , 'getProductType']);
+// Route::get('test', [UserController::class , 'xinchao']);
+// Route::get('sum', [SumController::class , 'GetValue']);
+// Route::post('sum', [SumController::class , 'Result']);
+// Route::get('Signup', [SignupController::class , 'index']);
+// Route::post('Signup', [SignupController::class , 'displayInfor']);
+// Route::get('addRoom', [RoomController::class , 'index']);
+// Route::post('addRoom', [RoomController::class , 'addRoom']);
+// Route::get('home', [PageController::class , 'getIndex']);
+// Route::get('home', [PageController::class , 'getProductType']);
+
+
+
+Route::get('/', [createTable::class , 'createTable']);
+
+
